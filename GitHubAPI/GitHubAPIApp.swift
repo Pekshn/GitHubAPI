b669cab8fafb75b0094ce53b550af4272c29de8a
Petar <iosdev.petar@gmail.com>
2025-03-21 13:10:24 +0100

    Initial commit.

---
+++ GitHubAPI/GitHubAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAPIApp.swift
+//  GitHubAPI
+//
+//  Created by Petar  on 21.3.25..
+//
+
+import SwiftUI
+
+@main
+struct GitHubAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
